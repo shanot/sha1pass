b23791b788dd8b7b67d235e5cf8fcc2eb79ef371
samuel hanot <smlhnt@gmail.com>
2015-12-04 16:25:43 +0100

    added show-hide button

--- sha1pass.cc
+++ sha1pass.cc
@@ -9,24 +9,38 @@ struct Hash {
 
 class Sha1pass : public Gtk::Window {
 public:
-  Sha1pass(const std::vector<Hash>& in) : buttons(in.size()), hash(in) {
+  Sha1pass(const std::vector<Hash> &in) : buttons(in.size()), hash(in) {
     add(grid);
     auto ewidth = buttons.size() / 2;
 
-    grid.attach(salt, 0, 0, ewidth, 1);
-    grid.attach(key, ewidth, 0, ewidth, 1);
+    auto row = 0;
+    grid.attach(salt, 0, row, ewidth, 1);
+    grid.attach(key, ewidth, row, ewidth, 1);
+    show.set_active(false);
+    salt.set_visibility(show.get_active());
+    key.set_visibility(show.get_active());
 
+    ++row;
+    grid.attach(show, 0, row, 1, 1);
+    show.set_label("show");
+    show.signal_clicked().connect([&]() {
+      salt.set_visibility(show.get_active());
+      key.set_visibility(show.get_active());
+    });
+
+    ++row;
     for (auto i(0); i < hash.size(); ++i) {
       buttons[i].set_label(hash[i].name);
       buttons[i].signal_clicked().connect({[=]() {
         const auto in = salt.get_text() + key.get_text();
         std::stringstream cmd;
-        cmd << "echo -n " << in << " | openssl sha1 " << hash[i].hashcmd << " | xclip";
+        cmd << "echo -n " << in << " | openssl sha1 " << hash[i].hashcmd
+            << " | xclip";
         if (system(cmd.str().c_str())) {
           abort();
         }
       }});
-      grid.attach(buttons[i], i, 1, 1, 1);
+      grid.attach(buttons[i], i, row, 1, 1);
     }
     show_all_children();
   }
@@ -36,6 +50,7 @@ private:
   Gtk::Grid grid;
   Gtk::Entry salt;
   Gtk::Entry key;
+  Gtk::CheckButton show;
   std::vector<Gtk::Button> buttons;
   std::vector<Hash> hash;
 };
@@ -43,9 +58,9 @@ private:
 int main(int argc, char *argv[]) {
   auto app = Gtk::Application::create(argc, argv, "org.sha1pass");
 
-  Hash hex{"HEX",           "-hex -r | cut -d ' ' -f 1 | tr -d '\n'"};
-  Hash hex_h{"HEX half",    "-hex -r | cut -d ' ' -f 1 | head -c 20"};
-  Hash b64{"Base64",        "-binary | openssl base64  | tr -d '\n'"};
+  Hash hex{"HEX", "-hex -r | cut -d ' ' -f 1 | tr -d '\n'"};
+  Hash hex_h{"HEX half", "-hex -r | cut -d ' ' -f 1 | head -c 20"};
+  Hash b64{"Base64", "-binary | openssl base64  | tr -d '\n'"};
   Hash b64_h{"Base64 half", "-binary | openssl base64  | head -c 14"};
 
   Sha1pass sha1pass{{hex, hex_h, b64, b64_h}};
